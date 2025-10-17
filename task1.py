d1313507279b5ea7aa707cc462e72d87225fcbf9
AnnaKulai79 <hannahku68@gmail.com>
2025-10-17 14:58:26 +0200

    task1 + task1dz

---
+++ task1.py
@@ -0,0 +1,21 @@
+# Сума двох чисел: Написати програму, яка зчитує два цілих числа та виводить їх суму.
+n1 = int(input("Enter first integer number1: "))
+n2 = int(input("Enter second integer number2: "))
+print("The sum of your numbers is", n1 + n2)
+
+# Гіпотенуза: За двома катетами прямокутного трикутника знайти довжину його гіпотенузи.
+leg1 = int(input("Enter first leg1: "))
+leg2 = int(input("Enter second leg2: "))
+print("The hypotenuse is", ( leg1 ** 2 + leg2 ** 2 ) ** 0.5)
+
+# Двічі: Написати програму, яка зчитує ціле число та виводить його двічі.
+num = int(input("Enter integer number: "))
+print(num,",",num)
+
+# Привіт, <ім'я>: Написати програму, яка запитує ім'я користувача і виводить привітання.
+name1 = input("Enter your name: ")
+print("Hello ", name1, "!")
+
+# Прощавай, <ім'я>: Написати програму, яка запитує ім'я користувача і виводить прощання.
+name1 = input("Enter your name: ")
+print("Goodbye ", name1, "!")
